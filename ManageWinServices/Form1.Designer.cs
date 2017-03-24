3cf6e0d8bc724f3830d821f60d1cfbce124dc486
Asif Khan Pathan <Asif.Pathan@asos.com>
2017-03-24 10:43:29 +0000

    Checking in MageServiceProject

---
+++ ManageWinServices/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ManageWinServicese
+{
+    partial class RdpServicesStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.servicesStatusDetailsGrid = new System.Windows.Forms.DataGridView();
+            this.txtServerNameValidator = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnStopService = new System.Windows.Forms.Button();
+            this.btnStartService = new System.Windows.Forms.Button();
+            this.btnStartAll = new System.Windows.Forms.Button();
+            this.btnStopAll = new System.Windows.Forms.Button();
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.servicesStatusDetailsGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtServerNameValidator)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(141, 25);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(349, 22);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Validating += new System.ComponentModel.CancelEventHandler(this.txtServer_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Server Name";
+            // 
+            // servicesStatusDetailsGrid
+            // 
+            this.servicesStatusDetailsGrid.AllowUserToAddRows = false;
+            this.servicesStatusDetailsGrid.AllowUserToDeleteRows = false;
+            this.servicesStatusDetailsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.servicesStatusDetailsGrid.Location = new System.Drawing.Point(4, 63);
+            this.servicesStatusDetailsGrid.Name = "servicesStatusDetailsGrid";
+            this.servicesStatusDetailsGrid.ReadOnly = true;
+            this.servicesStatusDetailsGrid.RowTemplate.Height = 24;
+            this.servicesStatusDetailsGrid.Size = new System.Drawing.Size(646, 647);
+            this.servicesStatusDetailsGrid.TabIndex = 9;
+            // 
+            // txtServerNameValidator
+            // 
+            this.txtServerNameValidator.ContainerControl = this;
+            // 
+            // btnStopService
+            // 
+            this.btnStopService.Location = new System.Drawing.Point(8, 73);
+            this.btnStopService.Name = "btnStopService";
+            this.btnStopService.Size = new System.Drawing.Size(115, 32);
+            this.btnStopService.TabIndex = 10;
+            this.btnStopService.Text = "Stop Service";
+            this.btnStopService.UseVisualStyleBackColor = true;
+            this.btnStopService.Click += new System.EventHandler(this.btnStopService_Click);
+            // 
+            // btnStartService
+            // 
+            this.btnStartService.Location = new System.Drawing.Point(6, 30);
+            this.btnStartService.Name = "btnStartService";
+            this.btnStartService.Size = new System.Drawing.Size(115, 31);
+            this.btnStartService.TabIndex = 11;
+            this.btnStartService.Text = "Start Service";
+            this.btnStartService.UseVisualStyleBackColor = true;
+            this.btnStartService.Click += new System.EventHandler(this.btnStartService_Click);
+            // 
+            // btnStartAll
+            // 
+            this.btnStartAll.Location = new System.Drawing.Point(6, 21);
+            this.btnStartAll.Name = "btnStartAll";
+            this.btnStartAll.Size = new System.Drawing.Size(115, 32);
+            this.btnStartAll.TabIndex = 12;
+            this.btnStartAll.Text = "Start All";
+            this.btnStartAll.UseVisualStyleBackColor = true;
+            this.btnStartAll.Click += new System.EventHandler(this.btnStartAll_Click);
+            // 
+            // btnStopAll
+            // 
+            this.btnStopAll.Location = new System.Drawing.Point(6, 62);
+            this.btnStopAll.Name = "btnStopAll";
+            this.btnStopAll.Size = new System.Drawing.Size(115, 32);
+            this.btnStopAll.TabIndex = 13;
+            this.btnStopAll.Text = "Stop All";
+            this.btnStopAll.UseVisualStyleBackColor = true;
+            this.btnStopAll.Click += new System.EventHandler(this.btnStopAll_Click);
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(520, 19);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(130, 35);
+            this.btnGetAll.TabIndex = 14;
+            this.btnGetAll.Text = "Get All";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStopService);
+            this.groupBox1.Controls.Add(this.btnStartService);
+            this.groupBox1.Location = new System.Drawing.Point(656, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(159, 114);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "By Selection";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStartAll);
+            this.groupBox2.Controls.Add(this.btnStopAll);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Location = new System.Drawing.Point(656, 199);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(159, 115);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "All";
+            // 
+            // RdpServicesStatus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 790);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGetAll);
+            this.Controls.Add(this.servicesStatusDetailsGrid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServer);
+            this.Name = "RdpServicesStatus";
+            this.Text = "RDP Services Status";
+            this.Load += new System.EventHandler(this.RdpServicesStatus_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.servicesStatusDetailsGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtServerNameValidator)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView servicesStatusDetailsGrid;
+        private System.Windows.Forms.ErrorProvider txtServerNameValidator;
+        private System.Windows.Forms.Button btnStopService;
+        private System.Windows.Forms.Button btnStartService;
+        private System.Windows.Forms.Button btnStopAll;
+        private System.Windows.Forms.Button btnStartAll;
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ManageWinServices/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+using System.Security;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading;
+using System.ServiceProcess;
+
+
+namespace ManageWinServicese
+{
+    public partial class RdpServicesStatus : Form
+    {
+        private string _serverName;
+
+        private List<string> _serviceList;
+        public RdpServicesStatus()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Name: GetServicesList
+        /// Input parameters : None
+        /// Purpose : This method adds all the services names which we want to be running on the given app server. 
+        ///           It will return the List containing the all the service names.
+        /// Date Created : 15/03/2017
+        /// </summary>
+        /// <returns>List of String </returns>
+        private static List<string> GetServicesList()
+        {
+            var chkServicesList = new List<string>
+            {
+                "Asos.Commerce.Orders.Endpoint",
+                "Asos.Commerce.Orders.Endpoint-HIGH",
+                "Asos.Commerce.Orders.Endpoint-LOW",
+                "Asos.Commerce.Orders.Endpoint-MEDIUM",
+                "Asos.Fulfilment.Delivery.BuyersRemorse.Endpoint",
+                "Asos.Fulfilment.Delivery.Scheduling.Endpoint",
+                "Asos.Finance.Payments.Card.Endpoint"
+            };
+
+
+            return chkServicesList;
+        }
+
+     private void SetDataGridPropertise()
+        {
+            servicesStatusDetailsGrid.DataSource = GetStatustable();
+            servicesStatusDetailsGrid.ReadOnly = true;
+            servicesStatusDetailsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            servicesStatusDetailsGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+
+        /// <summary>
+        /// Name: GetStatustable
+        /// Input parameters : None
+        /// Purpose : This method will create the table with Service Name & Status as columns to display in DataGridView
+        /// Date Created : 15/03/2017
+        /// </summary>
+        /// <returns> DataTable</returns>
+        private DataTable GetStatustable()
+        {
+            var statusDataTable = new DataTable();
+
+            statusDataTable.Columns.Add("Service Name");
+            statusDataTable.Columns.Add("Status");
+
+            try
+            {
+                if (_serviceList.Any())
+                {
+                    foreach (var record in _serviceList)
+                    {
+                        statusDataTable.Rows.Add(record, CheckServiceStatus(record));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return statusDataTable;
+        }
+
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(_serverName)) return;
+            ShowWaitCursor(true);
+
+            try
+            {
+                  ServiceController[] servicesOnServer;
+
+                servicesOnServer = ServiceController.GetServices(_serverName).Where(x => x.ServiceName.ToLower().Contains("ASOS".ToLower())).ToArray();
+
+                if (servicesOnServer.Any())
+                {
+                    _serviceList.Clear();
+                    foreach (var service in servicesOnServer)
+                    {
+                        _serviceList.Add(service.ServiceName);
+                    }
+                }
+                
+                SetDataGridPropertise();
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                ShowWaitCursor(false);
+            }
+
+        }
+
+        /// <summary>
+        /// Name: CheckServiceStatus
+        /// Input parameters : Name of the service whose status needs to be checked
+        /// Purpose : This method will check and returns the status of the given service on the given app server
+        /// Date Created : 15/03/2017
+        /// </summary>
+        /// <returns> String describing current service status</returns>
+        private string CheckServiceStatus(string serviceName)
+        {
+            try
+            {
+                var serviceController = new ServiceController(serviceName, _serverName);
+                return serviceController.Status.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+            
+        }
+
+        /// <summary>
+        /// Name: RunService
+        /// Input parameters : Name of the service which needs to be started
+        /// Purpose : This method will check if the given service is running, if NOT RUNNING then will start it on the given app server
+        /// Date Created : 15/03/2017
+        /// </summary>
+        /// <returns> Bool representing current service status</returns>
+        private bool RunService(string serviceName)
+        {
+            var serviceController = new ServiceController(serviceName, _serverName);
+            bool serviceStatus = false;
+            if (serviceController.Status == ServiceControllerStatus.Running) return serviceStatus;
+            try
+            {
+                serviceController.Start();
+                serviceStatus = true;
+            }
+            catch (Exception ex)
+            {
+                WaitForServiceToPerformOperations(1000);
+                try
+                {
+                    serviceController.Start();
+                    serviceStatus= true;
+                }
+                catch (Exception secex)
+                {
+                    Console.WriteLine(secex);
+                }
+            }
+
+            return serviceStatus;
+        }
+
+        /// <summary>
+        /// Name: StartService
+        /// Input parameters : Name of the service which needs to be started
+        /// Purpose : This method will check if the given service is in running status, if NOT RUNNING then will start it on the given app server
+        /// Date Created : 15/03/2017
+        /// </summary>
+        private void StartService(string serviceName)
+        {
+            var serviceController = new ServiceController(serviceName, _serverName);
+            if (serviceController.Status == ServiceControllerStatus.Running) return;
+            else
+            {
+                serviceController.Start();
+                WaitForServiceToPerformOperations(3000);
+
+                var retryCount = 0;
+                while (retryCount < 15)
+                {
+                    if (serviceController.Status != ServiceControllerStatus.Running)
+                        WaitForServiceToPerformOperations(1000);
+                    retryCount += 1;
+                }
+
+                //if (serviceController.Status != ServiceControllerStatus.Running)
+                //{
+                //    //Add code to check the Error Log
+                //}
+            }
+            
+        }
+
+        private void RestartService(string serviceName)
+        {
+            var serviceController = new ServiceController(serviceName, _serverName);
+            if (serviceController.Status == ServiceControllerStatus.Running)
+            {
+                serviceController.Stop();
+                WaitForServiceToPerformOperations(3000);
+            }
+            serviceController.Start();
+        }
+
+        private void WaitForServiceToPerformOperations(int timeInMilliseconds)
+        {
+            Thread.Sleep(timeInMilliseconds);
+        }
+
+        private string GetLastException( string provider)
+        {
+
+            string result = "";
+            const string Query =
+                ("<QueryList> <Query Id='0' Path='Application'> <Select Path='Application'>*[System[(Level=2)]]</Select></Query></QueryList>");
+
+            EventLogReader eventLogReader = new EventLogReader(new EventLogQuery("Application", PathType.LogName, Query)
+            {
+                ReverseDirection = true,
+                Session = new EventLogSession(_serverName)
+            });
+            while (true)
+            {
+                try
+                {
+                    EventRecord eventRecord = eventLogReader.ReadEvent();
+                    if (eventRecord != null && eventRecord.Properties.Count > 0 &&
+                        eventRecord.ProviderName == provider)
+                    {
+                        result = eventRecord.Properties[0].Value.ToString();
+                        break;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    result = "Exception not logged on Server";
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private void txtServer_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _serverName = txtServer.Text;
+
+            if (string.IsNullOrWhiteSpace(_serverName))
+            {
+                txtServerNameValidator.SetError(txtServer,
+                    "Oops you forgot to tell which app server you want to connect");
+            }
+            else
+            {
+                txtServerNameValidator.Clear();
+            }
+        }
+
+        private void btnStopService_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (servicesStatusDetailsGrid.Rows.Count <= 0 || string.IsNullOrWhiteSpace(servicesStatusDetailsGrid.CurrentCell.Value.ToString()))
+                    txtServerNameValidator.SetError(servicesStatusDetailsGrid, "No Services record available");
+                else
+                {
+                    txtServerNameValidator.Clear();
+                    ShowWaitCursor(true);
+
+                    var selectedCells = servicesStatusDetailsGrid.SelectedCells;
+
+                    if (selectedCells.Count > 0)
+                        foreach (DataGridViewCell cell in selectedCells)
+                    {
+                        StopService(servicesStatusDetailsGrid.Rows[cell.RowIndex].Cells[0].Value.ToString());
+                    }
+
+                    SetDataGridPropertise();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                ShowWaitCursor(false);
+            }
+        }
+
+        /// <summary>
+        /// Name: StopService
+        /// Input parameters : Name of the service which needs to be stopped
+        /// Purpose : This method will check if the given service is not running, if  RUNNING then will stop it on the given app server
+        /// Date Created : 16/03/2017
+        /// </summary>
+        /// <returns> Bool representing current service status</returns>
+        private void StopService(string serviceName)
+        {
+            var serviceController = new ServiceController(serviceName, _serverName);
+            if (serviceController.Status == ServiceControllerStatus.Stopped) return;
+            else
+            {
+                serviceController.Stop();
+                WaitForServiceToPerformOperations(3000);
+
+                var retryCount = 0;
+                while (retryCount < 15)
+                {
+                    if (serviceController.Status != ServiceControllerStatus.Stopped)
+                        WaitForServiceToPerformOperations(1000);
+                    retryCount += 1;
+                }
+
+                //if (serviceController.Status != ServiceControllerStatus.Stopped)
+                //{
+                //    //Add code to check the Error Log
+                //}
+            }
+        }
+
+
+        private void btnStartService_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_serverName)) return;
+            ShowWaitCursor(true);
+
+            try
+            {
+                var selectedCells = servicesStatusDetailsGrid.SelectedCells;
+
+                if(selectedCells.Count>0)
+                foreach (DataGridViewCell cell in selectedCells)
+                {
+                    StartService(servicesStatusDetailsGrid.Rows[cell.RowIndex].Cells[0].Value.ToString());
+                    WaitForServiceToPerformOperations(1000);
+                }
+
+                SetDataGridPropertise();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                ShowWaitCursor(false);
+            }
+        }
+
+        private void ShowWaitCursor(bool isWait)
+        {
+            if(isWait)
+                Cursor = Cursors.WaitCursor;
+            else
+                Cursor = Cursors.Default;
+        }
+
+        private void RdpServicesStatus_Load(object sender, EventArgs e)
+        {
+            _serviceList = new List<string>();
+            _serviceList = GetServicesList();
+        }
+
+        private void btnStartAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowWaitCursor(true);
+                foreach (var service in _serviceList)
+                    {
+                        StartService(service);
+                    }
+
+                SetDataGridPropertise();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                ShowWaitCursor(false);
+            }
+        }
+
+        private void btnStopAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowWaitCursor(true);
+                foreach (var service in _serviceList)
+                {
+                    StopService(service);
+                }
+
+                SetDataGridPropertise();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                ShowWaitCursor(false);
+            }
+        }
+    }
+}
